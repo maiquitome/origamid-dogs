49d76f5acbb42af480e35be5665bfcc4755e72f2
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-10 07:14:36 -0300

    feat: :sparkles: add initial component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+import "./App.css";
+
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
